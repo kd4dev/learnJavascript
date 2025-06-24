e36bfda59afbb8d4a87c9af9b16da7deba673173
kedarBaswade <kedarbaswade2802@gmail.com>
2025-06-24 23:36:24 +0530

    added about dataTypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,20 @@
-const accountId=24456
-let accountEmail="hitesh@google.com"
-var accountPassword="12345"
-accountCity="Jaipur"
+const accountId = 24456; //not allowed to change
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+//accountId=2 -->not allowed to change
+
+accountEmail = "kedar@google.com";
+accountPassword = "9999";
+accountCity = "nanded";
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+prefer not  to use var beacuase
+ of issue in block and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all js as a newer version code
+
+/*
+pehli ki js alag thi,naya code aur purane me farak pata chale is liye ye aaya
+abhi ye nahi likha toh bhi chalta he.
+*/
+
+// alert(3+3) -->ye nodejs he ye sab broweser me chlta he ise use krne ka syntax alag he
+
+//code readabilty should be high
+// ; --> semi-colon use krna na krna hamari marji he
+
+
+let name="kedar" //string-->" "--> ' ' -->dono chalte he-->use only one
+let age=19 //number -->Range-->2^53 - 1 se 2^53 - 1
+let isLoggedIn=false  // boolean
+let temparature=null //abhi tak temp aya hi nahi
+
+console.log(typeof null)  //object
+console.log(typeof undefined)  //undefined 
+
+//typeof is used to find type of any data type
+
+/*   <--primitive data types-->
+//number
+//string
+//boolean
+//bigint -->range-->-2^100 to 2^100 
+//null-->stand alone value
+//undefined -->value ayi hi nahi
+//symbol --> ye react me,for uniquness
+
+*/
