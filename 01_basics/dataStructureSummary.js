f090d958ed32c4c1a36d248dc12d35291fe734d9
kedarBaswade <kedarbaswade2802@gmail.com>
2025-06-25 17:23:08 +0530

    added summary of dataTypes

---
+++ 01_basics/dataStructureSummary.js
@@ -0,0 +1,32 @@
+//js is dynamically typed,their is no type safety as typescript
+//primitive-->passed by values
+//String,Number,Boolean,null,undefined,Symbol,Bigint
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+//non-primitive-->passed by reference
+//Array,Objects,Functions
+const heroes=["spider man","iron man","thor"]
+let myObj={
+    name:"kedar",
+    age:19
+}
+
+const myFunction=function(){
+    console.log("hello world")
+}
+
+/* Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+ Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object           */
