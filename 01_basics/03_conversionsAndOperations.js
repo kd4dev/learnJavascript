d5baf59d180820e54f316bdac82dddd6c1187a76
kedarBaswade <kedarbaswade2802@gmail.com>
2025-06-25 15:56:46 +0530

    added about conversions and opeartionss-2

--- 01_basics/03_conversionsAndOperations.js
+++ 01_basics/03_conversionsAndOperations.js
@@ -35,3 +35,35 @@ console.log(value3) //NaN
 
 // 0 , "" (empty string) --> convert Boolean -->false
 // any non-zero number,"kedar"(any non empty string) --> convert Boolean --> true
+
+// *****************************OPERATIONS***************************************
+
+let value5=3
+let negValue=-value5
+console.log(negValue)
+console.log(3+2) //5
+console.log(3-2) //1
+console.log(3*2) //6
+console.log(3**2) //9-->a**b-->a^b
+console.log(3/2) //1.5
+console.log(3%2)//1
+console.log(2%3)//2-->a%b-->a if(a<b
+
+let str1="hello"
+let  str2="world"
+let str3=str1+str2
+console.log(str3) //helloworld
+console.log(typeof("1"+2)) //string
+console.log(1+"2")//12
+console.log("1"+2+2) //122 -->string
+console.log(1+2+"2")//32-->string
+console.log(+true)//1
+console.log(true+1)//2
+console.log(+"")//0
+
+//focus more on readability not on fancieness
+
+let counter=100
+console.log(++counter)//101
+console.log(counter)//101
+console.log(counter++)//101
