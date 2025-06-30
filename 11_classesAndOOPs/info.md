505d8abc6f9c4381f15ff921204b7c3ea86e6045
kedarBaswade <kedarbaswade2802@gmail.com>
2025-06-30 23:55:55 +0530

    added about class,oops,getter/setters,objects,call,bind,this,new,proto inheritance,proto chaining,prototypes

---
+++ 11_classesAndOOPs/info.md
@@ -0,0 +1,25 @@
+# javascript and classes
+
+## OOP
+
+## Object
+- collection of properties and methods
+- toLowerCase
+
+## why use OOP
+- just a programing paradime
+
+## parts of OOP
+Object literal - literal just mean literally object hi le raha hu
+
+- Constructor function
+- Prototypes
+- Classes
+- Instances (new, this)
+
+
+## 4 pillars
+Abstraction(hiding details)
+Encapsulation(wrap up)
+Inheritance(passing)
+Polymorphism(many roots)
---
+++ 11_classesAndOOPs/oop.js
@@ -0,0 +1,41 @@
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this); //ye yahi object print kr dega
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
+
+//whenevr we use new keyword,an empty object is created called as instance
+//then costructor function is called due to new keyword
+//then packs all arguments inside it 
